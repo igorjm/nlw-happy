b617eb2f6330893bbe966344ad3aee186226a8b4
igor.melo <igorjmelo4@gmail.com>
2020-10-16 09:50:45 -0300

    init backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from './models/Orphanage';
+
+const routes = Router();
+
+routes.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json({message: 'Hello word'})
+});
+
+export default routes;
